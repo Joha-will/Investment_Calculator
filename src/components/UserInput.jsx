edb5faa7ec0f5c1504199f6c16c98eb3b54a3054
Joha Wilson <johawilsonatu@icloud.com>
2024-02-09 19:26:59 +0000

    Started creating handleChange function for user input

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -2,7 +2,24 @@ import { useState } from "react";
 
 export default function UserInput() {
 
-    const [userInput, setUserInput ] = useState(UserInput())
+    const [userInput, setUserInput ] = useState({
+
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+
+        setUserInput(prevUserInput => {
+
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            },
+        });
+    }
 
     return (
 
